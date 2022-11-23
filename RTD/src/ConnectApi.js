a12705cc5c41017b30f714102b7642508c26f3ea
A-RAMZI <73026384+A-RAMZI@users.noreply.github.com>
2022-11-23 15:13:34 +0100

    Beta

--- RTD/src/ConnectApi.js
+++ RTD/src/ConnectApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://192.168.1.2:8000/';
+const baseUrl = 'http://api-ocr-mobile.ipconnex.com/';
 
 export default axios.create({
     baseURL:baseUrl, 
